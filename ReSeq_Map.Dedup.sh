645388815dffc2a8e5a56bd2c98d1c5ca41f4648
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-01-11 20:20:39 +0100

    Create ReSeq_Map.Dedup.sh

---
+++ ReSeq_Map.Dedup.sh
@@ -0,0 +1,35 @@
+# Only change these variables: #################################################################
+export OUT_DIR="/scratch/ddepanis/Elephant_project/ReSeq/pilot"
+export ASSEMBLY="/scratch/ddepanis/Elephant_project/mEleMax1/intermediates/bionano/l2/l2_s1.fa"
+export ASSEMBLY_NAME=$(basename $ASSEMBLY .fa)
+export FQ_DIR="/scratch/ddepanis/Elephant_project/novaseq/trimmed"
+################################################################################################
+
+
+echo ""
+echo "=== STEP 1/3: Indexing ===================================="
+echo ""
+
+mkdir -p $OUT_DIR/idx
+INDEX_JOB=$(sbatch --output=$OUT_DIR/idx/%x.%j.out --error=$OUT_DIR/idx/%x.%j.err ReSeq.index.job)
+INDEX_JOB_ID=$(echo $INDEX_JOB | cut -d ' ' -f4)
+
+
+echo ""
+echo "=== STEP 2/3: Mapping/Sorting ============================="
+echo ""
+
+LENGTH=$(ls $FQ_DIR/*R1*.fastq.gz | wc -l)
+mkdir -p $OUT_DIR/bam_sort
+MAP_JOB=$(sbatch --dependency=afterok:$INDEX_JOB_ID --array=1-$LENGTH --output=$OUT_DIR/bam_sort/%x.%A_%a.out --error=$OUT_DIR/bam_sort/%x.%A_%a.err ReSeq_Map.Sort.job)
+MAP_JOB_ID=$(echo $MAP_JOB | cut -d ' ' -f4)
+
+
+echo ""
+echo "=== STEP 3/3: Deduplication  =============================="
+echo ""
+
+LENGTH=$(ls ${OUT_DIR}/bam_sort/*.bam | wc -l)
+mkdir -p $OUT_DIR/bam_dedup
+sbatch --dependency=afterok:$INDEX_JOB_ID:$MAP_JOB_ID --output=$OUT_DIR/bam_dedup/%x.%A_%a.out --error=$OUT_DIR/bam_dedup/%x.%A_%a.err ReSeq_Dedup.job
+                                                                                                                                                                                                        
