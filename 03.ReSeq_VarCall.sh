28df8874b3433f263d9fbea318d93aabc00c141e
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-01-17 00:21:36 +0100

    Update 03.ReSeq_VarCall.sh

--- 03.ReSeq_VarCall.sh
+++ 03.ReSeq_VarCall.sh
@@ -10,7 +10,7 @@ echo "=== STEP 1/x: Calling variants ===================================="
 echo ""
 
 mkdir -p $OUT_DIR/deepvar/intermediate_results_dir
-DEEPVAR_JOB=$(sbatch --output=$OUT_DIR/deepvar/%x.%j.out --error=$OUT_DIR/deepvar/%x.%j.err ReSeq_DeepVariant.job)
+DEEPVAR_JOB=$(sbatch --array=1-$LENGTH --output=$OUT_DIR/deepvar/%x.%A_%a.out --error=$OUT_DIR/deepvar/%x.%A_%a.err ReSeq_DeepVariant.job)
 DEEPVAR_JOB_ID=$(echo $DEEPVAR_JOB | cut -d ' ' -f4)
 
 echo ""
