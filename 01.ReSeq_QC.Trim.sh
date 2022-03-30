2d44a31f08ec509900898f4787943a29c654f19c
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-03-30 10:26:16 +0200

    Update 01.ReSeq_QC.Trim.sh

--- 01.ReSeq_QC.Trim.sh
+++ 01.ReSeq_QC.Trim.sh
@@ -15,13 +15,13 @@ FASTQC_JOB=$(sbatch --array=1-$LENGTH --output=${QC_DIR}/%x.%A_%a.out --error=${
 FASTQC_JOB_ID=$(echo $FASTQC_JOB | cut -d ' ' -f4)
 
 
-echo "=== STEP 2/3: Fastp     ======="
+echo "=== STEP 2/3: Fastp ======================================="
 
 mkdir -p $TRIM_DIR
 FASTP_JOB=$(sbatch --dependency=afterok:$FASTQC_JOB_ID --array=1-$LENGTH --output=${TRIM_DIR}/%x.%A_%a.out --error=${TRIM_DIR}/%x.%A_%a.err ReSeq_Fastp.job)
 FASTP_JOB_ID=$(echo $FASTP_JOB | cut -d ' ' -f4)
 
 
-echo "=== STEP 3/3: MultiQC   ======="
+echo "=== STEP 3/3: MultiQC ====================================="
 
-MULTI_JOB=$(sbatch --dependency=afterok:${FASTQC_JOB_ID}:${FASTP_JOB_ID} --output=${QC_DIR}/%x.%j.out --error=${QC_DIR}
+MULTI_JOB=$(sbatch --dependency=afterok:${FASTQC_JOB_ID}:${FASTP_JOB_ID} --output=${QC_DIR}/%x.%j.out --error=${QC_DIR}/%x.%j.err ReSeq_Multi.job)
