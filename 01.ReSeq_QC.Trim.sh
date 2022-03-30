ebcbc9f8b3cecce255419a2647a666bed5807e4f
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-03-30 15:46:57 +0200

    Update 01.ReSeq_QC.Trim.sh

--- 01.ReSeq_QC.Trim.sh
+++ 01.ReSeq_QC.Trim.sh
@@ -2,7 +2,6 @@
 export FQ_DIR="~/Elephant_project/ReSeq/genomic_data/novaseq"
 export QC_DIR="${FQ_DIR}/QC"
 export TRIM_DIR="${FQ_DIR}/trimmed"
-export LENGTH=$(ls $FQ_DIR/*R1* | wc -l)
 ################################################################################################
 
 
@@ -10,6 +9,7 @@ echo ""
 echo "=== STEP 1/3: FastQC ======================================"
 echo ""
 
+export LENGTH=$(ls $FQ_DIR/*R* | wc -l)
 mkdir -p $QC_DIR
 FASTQC_JOB=$(sbatch --array=1-$LENGTH --output=${QC_DIR}/%x.%A_%a.out --error=${QC_DIR}/%x.%A_%a.err slurm/ReSeq_FastQC.job)
 FASTQC_JOB_ID=$(echo $FASTQC_JOB | cut -d ' ' -f4)
@@ -17,6 +17,7 @@ FASTQC_JOB_ID=$(echo $FASTQC_JOB | cut -d ' ' -f4)
 
 echo "=== STEP 2/3: Fastp ======================================="
 
+export LENGTH=$(ls $FQ_DIR/*R1* | wc -l)
 mkdir -p $TRIM_DIR
 FASTP_JOB=$(sbatch --dependency=afterok:$FASTQC_JOB_ID --array=1-$LENGTH --output=${TRIM_DIR}/%x.%A_%a.out --error=${TRIM_DIR}/%x.%A_%a.err slurm/ReSeq_Fastp.job)
 FASTP_JOB_ID=$(echo $FASTP_JOB | cut -d ' ' -f4)
