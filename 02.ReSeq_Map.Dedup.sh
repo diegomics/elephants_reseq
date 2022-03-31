d0482028f118b309cf8c957e3a9ea5cf7af1388e
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-03-31 08:55:11 +0200

    Update 02.ReSeq_Map.Dedup.sh

--- 02.ReSeq_Map.Dedup.sh
+++ 02.ReSeq_Map.Dedup.sh
@@ -1,17 +1,11 @@
-# Only change and check these variables: #######################################################
-export OUT_DIR="~/Elephant_project/ReSeq/pilot"
-export ASSEMBLY="~/Elephant_project/mEleMax1/intermediates/bionano/l2/l2_s1.fa"
-export ASSEMBLY_NAME=$(basename $ASSEMBLY ${ASSEMBLY##*.})
-export FQ_DIR="~/Elephant_project/novaseq/trimmed"
-################################################################################################
-
+source variables.cnf
 
 echo ""
 echo "=== STEP 1/3: Indexing ===================================="
 echo ""
 
 mkdir -p ${OUT_DIR}/0_idx/logs
-INDEX_JOB=$(sbatch --output=${OUT_DIR}/0_idx/logs/%x.%j.out --error=${OUT_DIR}/0_idx/logs/%x.%j.err ReSeq.index.job)
+INDEX_JOB=$(sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/0_idx/logs/%x.%j.out --error=${OUT_DIR}/0_idx/logs/%x.%j.err ReSeq.index.job)
 INDEX_JOB_ID=$(echo $INDEX_JOB | cut -d ' ' -f4)
 
 
@@ -21,7 +15,7 @@ echo ""
 
 LENGTH=$(ls $FQ_DIR/*R1*.fastq.gz | wc -l)
 mkdir -p ${OUT_DIR}/1_bam_sort/logs
-MAP_JOB=$(sbatch --dependency=afterok:$INDEX_JOB_ID --array=1-$LENGTH --output=${OUT_DIR}/1_bam_sort/logs/%x.%A_%a.out --error=${OUT_DIR}/1_bam_sort/logs/%x.%A_%a.err ReSeq_Map.Sort.job)
+MAP_JOB=$(sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --dependency=afterok:$INDEX_JOB_ID --array=1-$LENGTH --output=${OUT_DIR}/1_bam_sort/logs/%x.%A_%a.out --error=${OUT_DIR}/1_bam_sort/logs/%x.%A_%a.err ReSeq_Map.Sort.job)
 MAP_JOB_ID=$(echo $MAP_JOB | cut -d ' ' -f4)
 
 
@@ -31,5 +25,5 @@ echo ""
 
 LENGTH=$(ls ${OUT_DIR}/1_bam_sort/*.bam | wc -l)
 mkdir -p ${OUT_DIR}/2_bam_dedup/logs
-sbatch --dependency=afterok:$INDEX_JOB_ID:$MAP_JOB_ID --output=${OUT_DIR}/2_bam_dedup/logs/%x.%A_%a.out --error=${OUT_DIR}/2_bam_dedup/logs/%x.%A_%a.err ReSeq_Dedup.job
+sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --dependency=afterok:$INDEX_JOB_ID:$MAP_JOB_ID --output=${OUT_DIR}/2_bam_dedup/logs/%x.%A_%a.out --error=${OUT_DIR}/2_bam_dedup/logs/%x.%A_%a.err ReSeq_Dedup.job
                                                                                                                                                                                                         
