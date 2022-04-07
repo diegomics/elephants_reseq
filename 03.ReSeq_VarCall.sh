a5fa4cc294c03e4db636f10b77625a40133d2781
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-04-07 12:52:36 +0200

    Update 03.ReSeq_VarCall.sh

--- 03.ReSeq_VarCall.sh
+++ 03.ReSeq_VarCall.sh
@@ -9,15 +9,15 @@ echo ""
 echo "=== STEP 1/x: Calling variants ===================================="
 echo ""
 
-LENGTH=$(ls $BAM_DIR/*.rmd.bam | wc -l)
-mkdir -p $OUT_DIR/tmp_dir
-DEEPVAR_JOB=$(sbatch --array=1-$LENGTH --output=$OUT_DIR/%x.%A_%a.out --error=$OUT_DIR/%x.%A_%a.err ReSeq_DeepVariant.job)
+LENGTH=$(ls ${BAM_DIR}/*.rmd.bam | wc -l)
+mkdir -p "${OUT_DIR}/tmp_dir"
+DEEPVAR_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --array=1-${LENGTH} --output=${OUT_DIR}/%x.%A_%a.out --error=${OUT_DIR}/%x.%A_%a.err ReSeq_DeepVariant.job)
 DEEPVAR_JOB_ID=$(echo $DEEPVAR_JOB | cut -d ' ' -f4)
 
 echo ""
 echo "=== STEP 2/x: Merge gVCF files ===================================="
 echo ""
 
-MERGE_JOB=$(sbatch --dependency=afterok:$DEEPVAR_JOB_ID --output=$OUT_DIR/%x.%j.out --error=$OUT_DIR/%x.%j.err ReSeq_GLnexus.job)
+MERGE_JOB=$(sbatch --dependency=afterok:$DEEPVAR_JOB_ID --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/%x.%j.out --error=$OUT_DIR/%x.%j.err ReSeq_GLnexus.job)
 MERGE_JOB_ID=$(echo $MERGE_JOB | cut -d ' ' -f4)
 
