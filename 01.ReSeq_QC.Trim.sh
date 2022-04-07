709ec4f141ef3ea8d28d2652ef58c73bb67addd3
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-04-07 12:50:07 +0200

    Update 01.ReSeq_QC.Trim.sh

--- 01.ReSeq_QC.Trim.sh
+++ 01.ReSeq_QC.Trim.sh
@@ -6,7 +6,7 @@ echo ""
 
 export LENGTH=$(ls $FQ_DIR/*R* | wc -l)
 mkdir -p ${QC_DIR}/logs
-FASTQC_JOB=$(sbatch --array=1-$LENGTH --output=${QC_DIR}/logs/%x.%A_%a.out --error=${QC_DIR}/logs/%x.%A_%a.err slurm/ReSeq_FastQC.job)
+FASTQC_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --array=1-$LENGTH --output=${QC_DIR}/logs/%x.%A_%a.out --error=${QC_DIR}/logs/%x.%A_%a.err slurm/ReSeq_FastQC.job)
 FASTQC_JOB_ID=$(echo $FASTQC_JOB | cut -d ' ' -f4)
 
 
@@ -14,10 +14,10 @@ echo "=== STEP 2/3: Fastp ======================================="
 
 export LENGTH=$(ls $FQ_DIR/*R1* | wc -l)
 mkdir -p ${TRIM_DIR}/logs
-FASTP_JOB=$(sbatch --dependency=afterok:$FASTQC_JOB_ID --array=1-$LENGTH --output=${TRIM_DIR}/logs/%x.%A_%a.out --error=${TRIM_DIR}/logs/%x.%A_%a.err slurm/ReSeq_Fastp.job)
+FASTP_JOB=$(sbatch --dependency=afterok:$FASTQC_JOB_ID --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --array=1-$LENGTH --output=${TRIM_DIR}/logs/%x.%A_%a.out --error=${TRIM_DIR}/logs/%x.%A_%a.err slurm/ReSeq_Fastp.job)
 FASTP_JOB_ID=$(echo $FASTP_JOB | cut -d ' ' -f4)
 
 
 echo "=== STEP 3/3: MultiQC ====================================="
 
-MULTI_JOB=$(sbatch --dependency=afterok:${FASTQC_JOB_ID}:${FASTP_JOB_ID} --output=${QC_DIR}/logs/%x.%j.out --error=${QC_DIR}/logs/%x.%j.err slurm/ReSeq_MultiQC.job)
+MULTI_JOB=$(sbatch --dependency=afterok:${FASTQC_JOB_ID}:${FASTP_JOB_ID} --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${QC_DIR}/logs/%x.%j.out --error=${QC_DIR}/logs/%x.%j.err slurm/ReSeq_MultiQC.job)
