92d3bced08d2d7f64d5c06b35a9028f4228afc93
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-04-21 13:52:42 +0200

    Update 03.ReSeq_VarCall.sh

--- 03.ReSeq_VarCall.sh
+++ 03.ReSeq_VarCall.sh
@@ -1,23 +1,17 @@
-# Only change these variables: #################################################################
-export OUT_DIR="~/Elephant_project/ReSeq/pilot/3_deepvar"
-export REF="~/Elephant_project/mEleMax1/intermediates/bionano/l2/l2_s1.fa"
-export BAM_DIR="~/Elephant_project/ReSeq/pilot/2_bam_dedup"
-################################################################################################
-
+source variables.cnf
 
 echo ""
-echo "=== STEP 1/x: Calling variants ===================================="
+echo "=== STEP 1/2: Calling variants ===================================="
 echo ""
 
-LENGTH=$(ls ${BAM_DIR}/*.rmd.bam | wc -l)
-mkdir -p "${OUT_DIR}/tmp_dir"
-DEEPVAR_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --array=1-${LENGTH} --output=${OUT_DIR}/%x.%A_%a.out --error=${OUT_DIR}/%x.%A_%a.err ReSeq_DeepVariant.job)
+LENGTH=$(ls ${OUT_DIR}/2_bam_dedup/*.rmd.bam | wc -l)
+mkdir -p "${OUT_DIR}/4_deepvar/logs"
+DEEPVAR_JOB=$(sbatch --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --array=1-${LENGTH} --output=${OUT_DIR}/4_deepvar/logs/%x.%A_%a.out --error=${OUT_DIR}/4_deepvar/logs/%x.%A_%a.err slurm/ReSeq_DeepVariant.job)
 DEEPVAR_JOB_ID=$(echo $DEEPVAR_JOB | cut -d ' ' -f4)
 
 echo ""
-echo "=== STEP 2/x: Merge gVCF files ===================================="
+echo "=== STEP 2/2: Merge gVCF files ===================================="
 echo ""
 
-MERGE_JOB=$(sbatch --dependency=afterok:$DEEPVAR_JOB_ID --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/%x.%j.out --error=$OUT_DIR/%x.%j.err ReSeq_GLnexus.job)
-MERGE_JOB_ID=$(echo $MERGE_JOB | cut -d ' ' -f4)
-
+MERGE_JOB=$(sbatch --dependency=afterok:$DEEPVAR_JOB_ID --mail-user=${USER_MAIL} --mail-type=${MAIL_TYPE} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/4_deepvar/logs/%x.%j.out --error=${OUT_DIR}/4_deepvar/logs/%x.%j.err slurm/ReSeq_GLnexus.job)
+#MERGE_JOB_ID=$(echo $MERGE_JOB | cut -d ' ' -f4)
