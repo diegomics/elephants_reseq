7a30707bb142cd400f22dbe9cd02e859d4c3a0a1
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-05-23 12:48:28 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,7 @@
 conda env create -f env.yml
+
+source variables.cnf
+${SINGULARITY_LOAD}
+BIN_VERSION="1.3.0"
+singularity pull docker://google/deepvariant:"${BIN_VERSION}"
+singularity pull docker://ghcr.io/dnanexus-rnd/glnexus:v1.4.1
